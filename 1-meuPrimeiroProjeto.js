d823085eb6cdea9f1f945085e1cfcbf1ffb81397
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-05-30 20:04:03 -0300

    melhorando

--- 1-meuPrimeiroProjeto.js
+++ 1-meuPrimeiroProjeto.js
@@ -1,7 +1,17 @@
-console.log("Meu primeiro programa JS: trabalhando com variaveis");
+/*console.log("Meu primeiro programa JS: trabalhando com variaveis");
 
 const idade = 26;
 
 console.log(idade);
 console.log(idade-6);
+*/
+console.log('O console está funcionando')
+
+var vel = 20;
+
+if (vel>50){
+    console.log(`Voc~e utrapassou a velocidade`)
+}else{
+    console.log(`Sua velocidade está ok ${vel}km`)
+}
 
