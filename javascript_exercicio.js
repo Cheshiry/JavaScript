2621885be5c8c5e59fbb7f3330ba572fabb2a962
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-06-06 16:19:00 -0300

    Update javascript_exercicio.js
    Exercício js

--- javascript_exercicio.js
+++ javascript_exercicio.js
@@ -1,1 +1,11 @@
 window.alert("funciona");
+
+function carregar(){
+var msg = window.document.getElementById("");
+var img = window.document.getElementById("");
+var data = new aDate();
+var hora =data.getHours();
+
+msg.innerHtml =
+
+}
