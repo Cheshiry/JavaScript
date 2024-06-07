0cf40744cacc3beba176eebc78b3ac949d7abc72
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-06-06 23:08:07 -0300

    teste

--- javascript_exercicio.js
+++ javascript_exercicio.js
@@ -1,1 +1,8 @@
-window.alert("funciona");
+function carregar() {
+    var msg = window.document.getElementById("msg");
+    var img = window.document.getElementById("img");
+    var data = new Date();
+    var hora = data.getHours
+
+    msg.innerHTML = "Agora são ${hora} "
+}
