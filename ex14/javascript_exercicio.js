d321dec84a99c0a37f7d6838c8b065fbf1a8347d
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-06-08 16:02:24 -0300

    Atualizar o javascript_exercicio.js
    if

--- ex14/javascript_exercicio.js
+++ ex14/javascript_exercicio.js
@@ -7,5 +7,16 @@ var data = new aDate();
 var hora =data.getHours();
 
 msg.innerHtml = "Agora são ${hora} "
+  if(hora >= 0 && hora < 12){
+    img.src = "";
+    document.body.style.background = "";
 
+}else if(hora >= 12 && hora <= 18){
+img.src = "";
+    document.body.style.background = "";
+
+}else{
+img.src = "";
+    document.body.style.background = "";
+}
 }
