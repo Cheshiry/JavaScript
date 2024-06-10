e75c32ad593c2ea8c0548074a7b6aa0b61790733
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-06-10 16:00:36 -0300

    Atualizar o javascript_exercicio.js
    mudar cor

--- ex14/javascript_exercicio.js
+++ ex14/javascript_exercicio.js
@@ -6,28 +6,17 @@ var img = window.document.getElementById("foto");
 var data = new Date();
 const hora = data.getHours();
 
-<<<<<<< HEAD
-msg.innerHTML = (`Agora são ${hora}`);
-
-/*if (hora >= 0 && hora < 12) {
-    
-} else if() {
-    
-=======
 msg.innerHtml = "Agora são ${hora} "
   if(hora >= 0 && hora < 12){
     img.src = "";
-    document.body.style.background = "";
+    document.body.style.background = "#515154";
 
 }else if(hora >= 12 && hora <= 18){
 img.src = "";
-    document.body.style.background = "";
+    document.body.style.background = "#e2cd9f";
 
 }else{
 img.src = "";
-    document.body.style.background = "";
-}
->>>>>>> d321dec84a99c0a37f7d6838c8b065fbf1a8347d
-}
+    document.body.style.background = "#b9846f";
 
-}*/
+}
