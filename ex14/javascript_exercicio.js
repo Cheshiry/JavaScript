be6653731bed4a87dd1a02a0a8758b792867c4ed
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-06-14 19:22:58 -0300

    agora está ok

--- ex14/javascript_exercicio.js
+++ ex14/javascript_exercicio.js
@@ -4,26 +4,27 @@ function carregar() {
 var msg = document.getElementById("mensagem");
 var img = document.getElementById("img");
 var data = new Date();
-const hora = data.getHours();
+const hora = 15;
+//data.getHours();
 
 
 msg.innerHTML = (`Agora são ${hora}`);
 
 if (hora >= 0 && hora < 12){
 
-    img.src ="C:\Users\agnes\OneDrive\Área de Trabalho\Alura Cursos\JavaScript\JavaScript\ex14\morning02.png"
+    img.src ="morning02.png";
     window.alert("manha");
     document.body.style.backgroundColor = "#fcf59c";
 
 }else if (hora >= 12 && hora <= 18) {
 
-    img.src = "C:\Users\agnes\OneDrive\Área de Trabalho\Alura Cursos\JavaScript\JavaScript\ex14\afternoon.png";
+    img.src = "afternoon.png";
     window.alert("tarde");
     document.body.style.backgroundColor = "#fd6b00";
 
 } else {
 
-    img.src = "C:\Users\agnes\OneDrive\Área de Trabalho\Alura Cursos\JavaScript\JavaScript\ex14\night.png";
+    img.src = "night.png";
     window.alert("noite");
     document.body.style.backgroundColor = "#100e24";
 }
