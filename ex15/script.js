19734a4cea7881f22ed049093301967f0829d39c
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-06-15 10:41:05 -0300

    formulario e o js não funciona verificar depois

--- ex15/script.js
+++ ex15/script.js
@@ -1,1 +1,16 @@
-1
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear();
+    var fano = document.getElementById("ano");
+    var res = document.getElementById("mensagem");
+
+    if (fano.ariaValueMax.length == 0 || (fano.value) > ano) {
+        window.alert("[E R R O] Verifique os dados!!");
+    } else {
+
+        var fsex = document.getElementsByName("radsex");
+        var idade = (ano - Number(fano.value));
+        res.innerHTML = (`Idade calculada: ${idade}`);
+    }
+
+}
