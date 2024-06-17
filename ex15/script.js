f68097fccd3686747c01223a432880fa9d0b2498
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-06-16 21:51:09 -0300

    Atualizar o script.js
    atualizado com o do celular

--- ex15/script.js
+++ ex15/script.js
@@ -1,16 +1,69 @@
-function verificar() {
+
+function ver() {
     var data = new Date();
     var ano = data.getFullYear();
-    var fano = document.getElementById("ano");
+    var formAno = document.getElementById("ano");
+    
     var res = document.getElementById("mensagem");
-
-    if (fano.ariaValueMax.length == 0 || (fano.value) > ano) {
-        window.alert("[E R R O] Verifique os dados!!");
+    
+    var img = document.createElement("img")
+    
+    if (Number(formAno.value) < 1900  ||  Number(formAno.value) >= ano) {
+      
+      window.alert("[ERRO] Digite um ano valido!!")
+      
     } else {
-
-        var fsex = document.getElementsByName("radsex");
-        var idade = (ano - Number(fano.value));
-        res.innerHTML = (`Idade calculada: ${idade}`);
+      var fsex = document.getElementsByName("radsex");
+      
+      var idade = (ano - Number(formAno.value));
+      
+      var genero = "";
+      
+      if (fsex[0].checked) {
+        genero = "Homem";
+        
+        if (idade >= 0 && idade< 10 ) {
+          //criança 
+      
+          img.setAttribute("src","morning02.png")
+          
+        } else if (idade< 21) {
+      
+          //adolescente jovem
+        img.setAttribute("src","morning02.png")  
+      
+        } else if (idade < 50) {
+          //adulto 
+          img.setAttribute("src","afternoon.png")
+        
+          
+        } else {
+          //idoso 
+          img.setAttribute("src","night.png")
+        }
+        
+      } else {
+        genero = "Mulher"
+      
+        
+        if (idade >= 0 && idade < 10 ) {
+          //criança 
+        } else if (idade< 21) {
+          //adolescente jovem
+        } else if (idade < 50) {
+          //adulto 
+        } else {
+          //idoso 
+        }
+        
+        
+        
+        
+      }
+      
     }
+        res.innerHTML = (`Você tem ${idade} e é ${genero}`);
+        
+        res.appendChil(img);
 
 }
